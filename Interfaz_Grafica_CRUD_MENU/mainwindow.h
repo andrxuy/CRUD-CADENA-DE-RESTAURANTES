0a4dd136be092c21a976e49443cdfe3cd74dcd6e
andrxuy <andresoto198@gmail.com>
2026-01-13 21:50:10 -0500

    Update mainwindow.h

--- Interfaz_Grafica_CRUD_MENU/mainwindow.h
+++ Interfaz_Grafica_CRUD_MENU/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vector>
+#include <string>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,6 +11,14 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+//DEFINIMOS NUESTRA ESTRUCTURA
+struct Menu {
+    int id;
+    std::string nombre;
+    std::string categoria;
+    std::string descripcion;
+    float precio;
+};
 
 class MainWindow : public QMainWindow
 {
