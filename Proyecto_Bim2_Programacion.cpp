6bfd92fdbce3368f5c242fab2f38e1c06fbc974a
andrxuy <andresoto198@gmail.com>
2026-01-19 13:53:54 -0500

    Cargar archivo Menu
    Función con archivos para cargar nuestro menú y modificaciones en el int main (){
    }

--- Proyecto_Bim2_Programacion.cpp
+++ Proyecto_Bim2_Programacion.cpp
@@ -1,4 +1,5 @@
 /* PROYECTO SEGUNDO BIMESTRE "MENUS EN CADENA DE RESTAURANTES"
+VERSIÓN DE ARCHIVOS
 Andrés Oto
 Matías Borja
 David Pilatuña*/
@@ -7,31 +8,59 @@ David Pilatuña*/
 #include<vector>
 #include<string>
 #include<algorithm>
+#include<fstream>
 #include <locale>
 using namespace std;
 //estructura
 
-struct Menu{
-	int id;
-	string nombre;
-	string descripcion;
-	string categoria;
-	double precio;
-	
-	void mostrar(){
-		cout << "\nID: " << id << "\n" << "Nombre: " << nombre << "\n"
-	    << "Descripción: " << descripcion << "\n"
-	    << "Categoría: " << categoria << "\n"
-	    << "Precio: $" << precio << "\n";
-	}	
+struct Menu {
+    int id;
+    string nombre;
+    string descripcion;
+    string categoria;
+    double precio;
 };
 
-void recorrer(vector<Menu>& menus){
-	for(Menu m:menus){
-		m.mostrar();
-	}
+vector<Menu> listaMenus;
+string ruta = "menus.txt";
+
+vector<Menu> cargarMenu() {
+    vector<Menu> menus;
+    ifstream archivo(ruta);
+    if (!archivo) {
+        cerr << "Archivo no encontrado. Se iniciará una nueva lista.\n";
+        return menus;
+    }
+
+    string linea;
+    while (getline(archivo, linea)) {
+        Menu m;
+        int pos = 0;
+        int campo = 0;
+        string token;
+
+        while ((pos = linea.find('|')) != string::npos) {
+            token = linea.substr(0, pos);
+            if (campo == 0) {
+                m.id = stoi(token);
+            } else if (campo == 1) {
+                m.nombre = token;
+            } else if (campo == 2) {
+                m.descripcion = token;
+            } else if (campo == 3) {
+                m.categoria = token;
+            }
+            linea.erase(0, pos + 1);
+            campo++;
+        }
+        m.precio = stod(linea);
+
+        menus.push_back(m);
+    }
+    return menus;
 }
 
+//LO DE ABAJO DESDE LA 64 HASTA LA 154, SIGUE SIENDO EN CONSOLA
 void agregar(vector<Menu>& menus) {
     Menu m;
     bool idDuplicado;
@@ -87,7 +116,6 @@ void buscar(vector<Menu>& menus) {
 }
 
 
-
 void actualizar(vector<Menu>& menus){
 	int idBuscar;
 	bool encontrado = false;
@@ -124,47 +152,47 @@ void actualizar(vector<Menu>& menus){
 	}
 }
 
-int main(){
-	setlocale(LC_ALL, "");
-	
-	int op=-1;
-	vector<Menu>menus; 
-	
-	while(op!=-0){
-	cout << "\n*=*=*=*=*=*=*=*=*=*APP REGISTRO MENÚS*=*=*=*=*=*=*=*=*=*"<<endl;
-	cout << "\n1. Mostrar Menús"<<endl;
-	cout << "2. Insertar nuevo Menú"<<endl;;
-	cout << "3. Actualizar Menú" << endl;
-	cout << "4. Buscar Menú"<<endl;
-	cout << "0. Salir del sistema"<<endl;;
-	cout << "Digite opcion: ";
-	cin >> op;	
-	
-	switch(op){
-		case 1:
-			recorrer(menus);
-			break;
-		case 2:
-			agregar(menus);
-			break;
-		case 3:
-			actualizar(menus);
-			break;
-		case 4:
-			buscar(menus);
-			break;	
-		case 0:
-			cout<<"\nSALIENDO DEL SISTEMA...."<<endl;
-			break;
-		default:
-			cout<<"\nOPCIÓN INCORRECTA"<<endl;		
-	}
-	}
-	
+//SOLO LA OPCION (case: 1) está con archivos, tomar en cuenta
+int main() {
+    setlocale(LC_ALL, "");
 
-	
-	return 0;
+    listaMenus = cargarMenu();
+
+    int opcion=-1;
+
+    while (true) {
+        cout << "\n*=*=*=*=*=*=*=*=*=* APP REGISTRO MENÚS *=*=*=*=*=*=*=*=*=*\n";
+        cout << "1. Registrar nuevo menú\n";
+        cout << "2. Buscar un menú\n";
+		cout << "3. Actualizar Menú" << endl;
+        //cout << "5. Mostrar todos los menús\n";
+        cout << "0. Salir\n";
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1: {
+                Menu nuevo = agregarMenu();
+                if (nuevo.id != 0) {  
+                    listaMenus.push_back(nuevo);
+                    guardarMenu();
+                }
+                break;
+			case 2:
+				buscar(menus);
+				break;
+			case 3:
+				actualizar(menus);
+				break;
+            case 0:
+                cout << "\nSALIENDO DEL SISTEMA...\n";
+                return 0;
+
+            default:
+                cout << "\nOPCIÓN INVÁLIDA. DÍGITE UNA OPCIÓN CORRECTA\n";
+        }
+    }
+    return 0;
 }
-
 
 
